77834de679fa042417a0b63a2763a0a5562bd8e0
Nh4nisme <nguyenhuunhan190495@gmail.com>
2025-12-17 22:16:27 +0700

    update the table in ThanhToan

--- src/main/java/com/thefourrestaurant/controller/HoaDonController.java
+++ src/main/java/com/thefourrestaurant/controller/HoaDonController.java
@@ -1,20 +1,32 @@
 package com.thefourrestaurant.controller;
 
 import com.thefourrestaurant.DAO.ChiTietHoaDonDAO;
+import com.thefourrestaurant.DAO.ChiTietPDBDAO;
 import com.thefourrestaurant.DAO.HoaDonDAO;
 import com.thefourrestaurant.model.ChiTietHoaDon;
+import com.thefourrestaurant.model.ChiTietPDB;
 import com.thefourrestaurant.model.HoaDon;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HoaDonController {
     private HoaDonDAO hoaDonDAO  = new HoaDonDAO();
     private ChiTietHoaDonDAO chiTietHoaDonDAO = new ChiTietHoaDonDAO();
+    private final ChiTietPDBDAO chiTietDAO = new ChiTietPDBDAO();
+
 
     public ObservableList<HoaDon> layDanhSachHoaDon(){
         return FXCollections.observableArrayList(hoaDonDAO.layDanhSachHoaDon());
+    }
+
+    public List<ChiTietPDB> layChiTietHienThi(String maPDB) {
+        List<ChiTietPDB> danhSachGoc = chiTietDAO.layTheoPhieu(maPDB);
+        return gopChiTietPhieu(danhSachGoc);
     }
 
     public List<ChiTietHoaDon> layCTHDTheoMa(String maHD){
@@ -30,5 +42,32 @@ public class HoaDonController {
     }
 
     public boolean xoaHoaDon(String maHD) {return hoaDonDAO.xoaHoaDon(maHD);}
+
+    private List<ChiTietPDB> gopChiTietPhieu(List<ChiTietPDB> danhSachGoc) {
+        Map<String, ChiTietPDB> danhSachGop = new LinkedHashMap<>();
+
+        for (ChiTietPDB ct : danhSachGoc) {
+            String khoa = ct.getMonAn() != null
+                    ? ct.getMonAn().getMaMonAn()
+                    : ct.getMaCT();
+
+            if (!danhSachGop.containsKey(khoa)) {
+                ChiTietPDB banSao = new ChiTietPDB(
+                        ct.getMaCT(),
+                        ct.getPhieuDatBan(),
+                        ct.getMonAn(),
+                        ct.getSoLuong(),
+                        ct.getDonGia(),
+                        ct.getGhiChu()
+                );
+                danhSachGop.put(khoa, banSao);
+            } else {
+                ChiTietPDB daCo = danhSachGop.get(khoa);
+                daCo.setSoLuong(daCo.getSoLuong() + ct.getSoLuong());
+            }
+        }
+        return new ArrayList<>(danhSachGop.values());
+    }
+
 }
 
