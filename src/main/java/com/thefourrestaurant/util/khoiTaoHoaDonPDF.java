84221c87c4030439d92143d921e50edea7ca7bf5
Nh4nisme <nguyenhuunhan190495@gmail.com>
2025-12-22 02:17:22 +0700

    update PDF invoice

--- src/main/java/com/thefourrestaurant/util/khoiTaoHoaDonPDF.java
+++ src/main/java/com/thefourrestaurant/util/khoiTaoHoaDonPDF.java
@@ -11,155 +11,168 @@ import java.text.DecimalFormat;
 
 public class khoiTaoHoaDonPDF {
 
-    public static void inHoaDon(String filePath,
-                                String maHD,
-                                String ngayNhan,
-                                NhanVien nv,
-                                KhachHang kh,
-                                ObservableList<ChiTietPDB> chiTietList,
-                                KhuyenMai khuyenMai,
-                                BigDecimal tienCoc,
-                                BigDecimal tienKhachDua,
-                                BigDecimal tienThua) {
+    public static void inHoaDon(
+            String filePath,
+            String maHD,
+            String ngayNhan,
+            NhanVien nv,
+            KhachHang kh,
+            ObservableList<ChiTietPDB> chiTietList,
+            KhuyenMai khuyenMai,
+            BigDecimal tienCoc,
+            BigDecimal tienKhachDua,
+            BigDecimal tienThua
+    ) {
         try {
+            // Tạo document
             Document document = new Document(PageSize.A4, 36, 36, 36, 36);
             PdfWriter.getInstance(document, new FileOutputStream(filePath));
             document.open();
 
-            Font titleFont = new Font(Font.HELVETICA, 18, Font.BOLD);
-            Font subTitleFont = new Font(Font.HELVETICA, 14, Font.BOLD);
-            Font normalFont = new Font(Font.HELVETICA, 12);
+            // Font
 
+            Font titleFont = new Font(Font.TIMES_ROMAN, 18, Font.BOLD);
+            Font subTitleFont = new Font(Font.TIMES_ROMAN, 14, Font.BOLD);
+            Font normalFont = new Font(Font.TIMES_ROMAN, 12);
             DecimalFormat df = new DecimalFormat("#,###");
 
             // ===== HEADER =====
-            PdfPTable headerTable = new PdfPTable(2);
-            headerTable.setWidthPercentage(100);
-            headerTable.setWidths(new int[]{3, 5});
+            Paragraph header = new Paragraph("THE FOUR RESTAURANT\n", titleFont);
+            header.setAlignment(Element.ALIGN_CENTER);
+            document.add(header);
 
-            // Logo + Tên nhà hàng
-            PdfPCell leftCell = new PdfPCell();
-            leftCell.setBorder(Rectangle.NO_BORDER);
-            Paragraph shopName = new Paragraph("THE FOUR RESTAURANT", titleFont);
-            leftCell.addElement(shopName);
-            headerTable.addCell(leftCell);
-
-            // Địa chỉ bên phải
-            PdfPCell rightCell = new PdfPCell();
-            rightCell.setBorder(Rectangle.NO_BORDER);
-            Paragraph address = new Paragraph("12 đường Nguyễn Văn Bảo, Phường 1, Gò Vấp, TP. HCM 700000", normalFont);
-            address.setAlignment(Element.ALIGN_RIGHT);
-            rightCell.addElement(address);
-            headerTable.addCell(rightCell);
-
-            document.add(headerTable);
-            document.add(new Paragraph(" ")); // khoảng cách
+            Paragraph address = new Paragraph(
+                    "12 Nguyễn Văn Bảo, Phường 1, Gò Vấp, TP.HCM 700000\n\n",
+                    normalFont
+            );
+            address.setAlignment(Element.ALIGN_CENTER);
+            document.add(address);
 
             // ===== THÔNG TIN HÓA ĐƠN =====
-            PdfPTable infoTable = new PdfPTable(2);
-            infoTable.setWidthPercentage(100);
-            infoTable.setSpacingBefore(10);
-            infoTable.setSpacingAfter(10);
+            Paragraph invoiceTitle = new Paragraph("HÓA ĐƠN THANH TOÁN\n\n", subTitleFont);
+            invoiceTitle.setAlignment(Element.ALIGN_CENTER);
+            document.add(invoiceTitle);
 
-            PdfPCell invoiceTitleCell = new PdfPCell(new Paragraph("HÓA ĐƠN THANH TOÁN", subTitleFont));
-            invoiceTitleCell.setColspan(2);
-            invoiceTitleCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            invoiceTitleCell.setBorder(Rectangle.NO_BORDER);
-            infoTable.addCell(invoiceTitleCell);
+            document.add(new Paragraph("Mã HĐ : " + maHD, normalFont));
+            document.add(new Paragraph("Ngày lập : " + ngayNhan, normalFont));
+            document.add(new Paragraph(
+                    "Khách hàng: " + (kh != null ? kh.getHoTen() : "Khách lẻ"), normalFont
+            ));
+            document.add(new Paragraph(
+                    "SĐT : " + (kh != null ? kh.getSoDT() : "-"), normalFont
+            ));
+            document.add(new Paragraph(
+                    "Nhân viên : " + (nv != null ? nv.getHoTen() : "-") + "\n\n", normalFont
+            ));
 
-            infoTable.addCell(new Phrase("Mã hóa đơn: " + maHD, normalFont));
-            infoTable.addCell(new Phrase("Ngày lập: " + ngayNhan, normalFont));
-            infoTable.addCell(new Phrase("Khách hàng: " + (kh != null ? kh.getHoTen() : "Khách lẻ"), normalFont));
-            infoTable.addCell(new Phrase("SĐT: " + (kh != null ? kh.getSoDT() : "-"), normalFont));
-            infoTable.addCell(new Phrase("Nhân viên lập: " + (nv != null ? nv.getHoTen() : "-"), normalFont));
-            infoTable.addCell(new Phrase(" ", normalFont)); // ô trống để cân bằng table
-
-            document.add(infoTable);
-
-            // ===== BẢNG MÓN =====
-            PdfPTable table = new PdfPTable(4);
+            // ===== DANH SÁCH MÓN =====
+            PdfPTable table = new PdfPTable(5);
             table.setWidthPercentage(100);
-            table.setWidths(new int[]{1, 5, 2, 3});
-            table.setSpacingBefore(10);
+            table.setSpacingBefore(10f);
+            table.setWidths(new float[]{1f, 5f, 2f, 3f, 3f}); // STT, Tên món, SL, Đơn giá, Thành tiền
 
-            table.addCell("STT");
-            table.addCell("Tên món");
-            table.addCell("Đơn giá");
-            table.addCell("Thành tiền");
+            // Header bảng
+            Font headerFont = new Font(Font.COURIER, 12, Font.BOLD);
+            table.addCell(new PdfPCell(new Phrase("STT", headerFont)));
+            table.addCell(new PdfPCell(new Phrase("Tên món", headerFont)));
+            table.addCell(new PdfPCell(new Phrase("SL", headerFont)));
+            table.addCell(new PdfPCell(new Phrase("Đơn giá", headerFont)));
+            table.addCell(new PdfPCell(new Phrase("Thành tiền", headerFont)));
 
-            int stt = 1;
+            // Nội dung món ăn
             BigDecimal tongTienChuaVAT = BigDecimal.ZERO;
-
+            int stt = 1;
             for (ChiTietPDB ct : chiTietList) {
-                table.addCell(String.valueOf(stt++));
-                table.addCell(ct.getMonAn().getTenMon());
-                table.addCell(df.format(ct.getDonGia()) + " đ");
-                BigDecimal thanhTien = BigDecimal.valueOf(ct.getDonGia()).multiply(BigDecimal.valueOf(ct.getSoLuong()));
-                table.addCell(df.format(thanhTien) + " đ");
+                BigDecimal thanhTien = BigDecimal.valueOf(ct.getDonGia())
+                        .multiply(BigDecimal.valueOf(ct.getSoLuong()));
+                table.addCell(new PdfPCell(new Phrase(String.valueOf(stt++), normalFont)));
+                table.addCell(new PdfPCell(new Phrase(ct.getMonAn().getTenMon(), normalFont)));
+                table.addCell(new PdfPCell(new Phrase(String.valueOf(ct.getSoLuong()), normalFont)));
+                table.addCell(new PdfPCell(new Phrase(df.format(ct.getDonGia()), normalFont)));
+                table.addCell(new PdfPCell(new Phrase(df.format(thanhTien), normalFont)));
                 tongTienChuaVAT = tongTienChuaVAT.add(thanhTien);
             }
-
             document.add(table);
+            document.add(new Paragraph("\n"));
 
-            // ===== THÔNG TIN THANH TOÁN =====
-            PdfPTable payTable = new PdfPTable(2);
-            payTable.setWidthPercentage(50);
-            payTable.setHorizontalAlignment(Element.ALIGN_RIGHT);
-            payTable.setSpacingBefore(10);
-
-            // Tổng tiền (chưa VAT)
-            payTable.addCell("Thành tiền:");
-            payTable.addCell(df.format(tongTienChuaVAT) + " đ");
+            // ===== TÍNH TOÁN =====
+            BigDecimal VAT = tongTienChuaVAT.multiply(BigDecimal.valueOf(0.1));
+            BigDecimal thanhToan = tongTienChuaVAT.add(VAT);
 
             // Khuyến mãi
+            String khuyenMaiStr = "0 đ";
             if (khuyenMai != null && khuyenMai.getKhuyenMaiDieuKien() != null) {
                 KhuyenMai_DieuKien dk = khuyenMai.getKhuyenMaiDieuKien();
-                if (dk.getTyLeGiam() != null) payTable.addCell("Khuyến mãi:");
-                else if (dk.getSoTienGiam() != null) payTable.addCell("Khuyến mãi:");
-                else payTable.addCell("Khuyến mãi:");
-            } else {
-                payTable.addCell("Khuyến mãi:");
+                if (dk.getTyLeGiam() != null) {
+                    khuyenMaiStr = dk.getTyLeGiam() + "%";
+                } else if (dk.getSoTienGiam() != null) {
+                    khuyenMaiStr = df.format(dk.getSoTienGiam()) + " đ";
+                }
             }
 
-            // Chiết khấu
-            if (khuyenMai != null && khuyenMai.getKhuyenMaiDieuKien() != null) {
-                KhuyenMai_DieuKien dk = khuyenMai.getKhuyenMaiDieuKien();
-                if (dk.getTyLeGiam() != null) payTable.addCell(dk.getTyLeGiam() + "%");
-                else if (dk.getSoTienGiam() != null) payTable.addCell(df.format(dk.getSoTienGiam()) + " đ");
-                else payTable.addCell("0 đ");
-            } else {
-                payTable.addCell("0 đ");
-            }
+            document.add(new Paragraph("------------------------------------------------------------------------", normalFont));
 
-            // VAT
-            BigDecimal VAT = tongTienChuaVAT.multiply(BigDecimal.valueOf(0.1));
-            payTable.addCell("VAT 10%:");
-            payTable.addCell(df.format(VAT) + " đ");
+            // ===== THÔNG TIN THANH TOÁN =====
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s", "Khuyến mãi", khuyenMaiStr), normalFont
+            ));
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s đ", "VAT 10%", df.format(VAT)), normalFont
+            ));
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s đ", "Tổng tiền phải thanh toán", df.format(thanhToan)), normalFont
+            ));
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s đ", "Tiền cọc", tienCoc != null ? df.format(tienCoc) : "0"), normalFont
+            ));
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s đ", "Tiền khách đưa", tienKhachDua != null ? df.format(tienKhachDua) : "0"), normalFont
+            ));
+            document.add(new Paragraph(
+                    String.format("%-25s : %12s đ", "Tiền dư", tienThua != null ? df.format(tienThua) : "0"), normalFont
+            ));
 
-            // Tiền phải thanh toán
-            BigDecimal thanhToan = tongTienChuaVAT.add(VAT);
-            payTable.addCell("Tổng tiền phải thanh toán:");
-            payTable.addCell(df.format(thanhToan) + " đ");
+            document.add(new Paragraph("------------------------------------------------------------------------", normalFont));
 
-            // Tiền cọc
-            payTable.addCell("Tiền cọc:");
-            payTable.addCell(df.format(tienCoc != null ? tienCoc : BigDecimal.ZERO) + " đ");
+            // ===== CHỮ KÝ =====
+            PdfPTable signatureTable = new PdfPTable(2);
+            signatureTable.setWidthPercentage(100);
+            signatureTable.setSpacingBefore(30f);
+            signatureTable.setWidths(new float[]{2.5f, 1f});
 
-            // Tiền khách đưa
-            payTable.addCell("Tiền khách đưa:");
-            payTable.addCell(df.format(tienKhachDua != null ? tienKhachDua : BigDecimal.ZERO) + " đ");
+            PdfPCell buyerCell = new PdfPCell();
+            buyerCell.setBorder(Rectangle.NO_BORDER);
+            buyerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            buyerCell.addElement(new Paragraph("Người mua", normalFont));
+            buyerCell.addElement(new Paragraph("(Ký và ghi rõ họ tên)", normalFont));
+            buyerCell.addElement(new Paragraph("\n\n\n", normalFont));
+            signatureTable.addCell(buyerCell);
 
-            // Tiền dư
-            payTable.addCell("Tiền dư:");
-            payTable.addCell(df.format(tienThua != null ? tienThua : BigDecimal.ZERO) + " đ");
+            PdfPCell sellerCell = new PdfPCell();
+            sellerCell.setBorder(Rectangle.NO_BORDER);
+            sellerCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            sellerCell.addElement(new Paragraph("Người bán", normalFont));
+            sellerCell.addElement(new Paragraph("(Ký và ghi rõ họ tên)", normalFont));
+            signatureTable.addCell(sellerCell);
 
-            document.add(payTable);
+            document.add(signatureTable);
+
+            document.add(new Paragraph("------------------------------------------------------------------------", normalFont));
 
             // ===== FOOTER =====
-            Paragraph footer = new Paragraph("Cảm ơn quý khách!", normalFont);
+            Paragraph footer = new Paragraph("\nCảm ơn quý khách!", normalFont);
             footer.setAlignment(Element.ALIGN_CENTER);
-            footer.setSpacingBefore(20);
             document.add(footer);
+
+            // ===== LỜI DẶN DÒ / THÂN THIỆN =====
+            Paragraph note = new Paragraph(
+                    "\nChúc quý khách một ngày vui vẻ! \n" +
+                            "Hãy giữ hóa đơn này để thuận tiện cho các giao dịch sau.\n" +
+                            "Mong được phục vụ quý khách lần sau!",
+                    normalFont
+            );
+            note.setAlignment(Element.ALIGN_CENTER);
+            document.add(note);
 
             document.close();
             System.out.println("In hóa đơn PDF thành công: " + filePath);
